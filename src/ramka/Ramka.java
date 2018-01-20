b9f2fa9fc1e09ffaa31d57248fb22b4af6cd4af5
Damian <Damian@DESKTOP-0SAAVPI>
2018-01-20 18:02:24 +0100

    + button

--- src/ramka/Ramka.java
+++ src/ramka/Ramka.java
@@ -10,27 +10,37 @@ import java.awt.*;
 public class Ramka extends JFrame
 {
     public Ramka()
-    {
-        this.setVisible(true);        
+    {     
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setTitle("Solid Deraking Team");
-        this.setSize(1280, 720);
+
 
         this.setIconImage(Toolkit.getDefaultToolkit().getImage(Ramka.class.getResource("logo.png")));
-//        this.pack();
+        initComponents(); //Inicjalizuje klasę, w której dodaje się komponenty
+        this.pack(); //Ustawia wielkość okna dopasowane do komponentów
+        this.setSize(1280, 720);
         this.setLocationRelativeTo(null); //Centruje ramkę, musi być zawsze po pack() i setSize()
     }
+ /**
+  * @since 1.01
+  * 
+  */   
     
-    
- public void initComponents()
- {
-     
-     
- }
+    public void initComponents()
+    {
+        buttonPageEnd = new JButton("Accept");
+        Container container = this.getContentPane(); //Pobieranie szybki i przypisanie jej do kontenera
+        container.setLayout(null);
+        container.add(buttonPageEnd);
+        buttonPageEnd.setSize(buttonPageEnd.getPreferredSize());
+        buttonPageEnd.setLocation(200, 200);
+    }
+ 
+    JButton buttonPageEnd;
     
     public static void main(String[] args) 
     {
-        new Ramka();
+        new Ramka().setVisible(true);
         
     }
     
