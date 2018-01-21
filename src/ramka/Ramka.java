b5bb9995d0e93a6322d0b4b497d16c08601a14de
Damian <Damian@DESKTOP-0SAAVPI>
2018-01-21 18:00:24 +0100

    Zmiana koloru tła przez wciśnięcie buttonu

--- src/ramka/Ramka.java
+++ src/ramka/Ramka.java
@@ -1,16 +1,17 @@
 package ramka;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 
 /**
- * @version 1.0
+ * @version 1.02
  * @author Damian
  */
 public class Ramka extends JFrame
 {
     public Ramka()
-    {     
+    {
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.setTitle("Solid Deraking Team");
 
@@ -28,15 +29,77 @@ public class Ramka extends JFrame
     
     public void initComponents()
     {
-        buttonPageEnd = new JButton("Accept");
-        Container container = this.getContentPane(); //Pobieranie szybki i przypisanie jej do kontenera
-        container.setLayout(null);
-        container.add(buttonPageEnd);
-        buttonPageEnd.setSize(buttonPageEnd.getPreferredSize());
-        buttonPageEnd.setLocation(200, 200);
+//        Container container = this.getContentPane(); //Pobieranie szybki i przypisanie jej do kontenera
+//        container.setLayout(null);
+//        container.add(buttonPageEnd);
+//        buttonPageEnd.setSize(buttonPageEnd.getPreferredSize());
+//        buttonPageEnd.setLocation(200, 200);
+        
+        /**
+         * @since 1.02
+         */
+        this.getContentPane().add(panel); 
+        GroupLayout layout = new GroupLayout(panel);
+        panel.setLayout(layout);
+        
+        layout.setHorizontalGroup( //Grupa pozioma
+                layout.createSequentialGroup()
+//                .addComponent(button1, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE) // 1. Minimalna wielkośc, 2. Preferowana wielkość, 3. Maksymalna wielkość
+//                .addGroup(
+//                    layout.createParallelGroup().
+//                            addComponent(button2, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE).
+//                            addComponent(button4, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+//                 )
+//                .addComponent(button3, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                .addContainerGap(10, Short.MAX_VALUE) //Przerwa, żeby umieścić przycisk na dole strony
+                .addComponent(buttonPageEnd, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+        );
+        
+        layout.setVerticalGroup( //Grupa pionowa
+                layout.createSequentialGroup()
+//                .addGroup(
+//                layout.createParallelGroup()
+//                        .addComponent(button1, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+//                        .addComponent(button2, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+//                        .addComponent(button3, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+//                )
+//                .addComponent(button4, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+                .addContainerGap(10, Short.MAX_VALUE) //Przerwa, żeby umieścić przycisk na dole strony
+                .addComponent(buttonPageEnd, GroupLayout.DEFAULT_SIZE , GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
+        );
+       
+        layout.setAutoCreateGaps(true); //Dodawanie odstępów między buttonami
+        layout.setAutoCreateContainerGaps(true); //Dodawanie odstępów między kontenerami
+        
+
+        buttonPageEnd.addActionListener(new colorListener(Color.BLACK));
+
     }
- 
-    JButton buttonPageEnd;
+    
+    private class colorListener implements ActionListener
+    {
+        public colorListener(Color color)
+        {
+            this.color = color;
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent event)
+        {
+            panel.setBackground(color);
+
+        }
+        
+        Color color;
+    }
+    
+    JPanel panel = new JPanel();
+    JButton buttonPageEnd = new JButton("Zmiana tła na kolor czarny"); //Zainicjowanie przycisku z napisem
+//    JButton button1 = new JButton("B1"); //Zainicjowanie przycisku z napisem
+//    JButton button2 = new JButton("B2"); //Zainicjowanie przycisku z napisem
+//    JButton button3 = new JButton("B3"); //Zainicjowanie przycisku z napisem
+//    JButton button4 = new JButton("B4"); //Zainicjowanie przycisku z napisem
+    
     
     public static void main(String[] args) 
     {
